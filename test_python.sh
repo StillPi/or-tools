51df3dafc19a5f0f534d56819be3cea6ee61e237
Corentin Le Molgat <corentinl@google.com>
2018-05-16 17:39:17 +0200

    Improve test_python.sh

--- test_python.sh
+++ test_python.sh
@@ -9,7 +9,11 @@ VENV_BIN_DIR=${VENV_DIR}/bin
 ${VENV_BIN_DIR}/python --version
 ${VENV_BIN_DIR}/python -m pip --version
 
-PYTHONPATH=ortools/gen ${VENV_BIN_DIR}/python -c "import ortools"
+PYTHONPATH=ortools ${VENV_BIN_DIR}/python -c "import ortools"
+PYTHONPATH=ortools ${VENV_BIN_DIR}/python -v -c "from ortools.linear_solver import pywraplp"
+
 cp test.py.in ${VENV_DIR}/test.py
-PYTHONPATH=ortools/gen ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py
+PYTHONPATH=ortools ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py
+#ldd ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
+#objdump -p ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
 #make install_python
