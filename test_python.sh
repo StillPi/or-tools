0a61d5c920aa1d069fe17f01f7ec54c142306aff
Corentin Le Molgat <corentinl@google.com>
2018-05-17 13:51:51 +0200

    fixup test_python.sh

--- test_python.sh
+++ test_python.sh
@@ -8,12 +8,16 @@ python -m virtualenv -p python ${VENV_DIR}
 VENV_BIN_DIR=${VENV_DIR}/bin
 ${VENV_BIN_DIR}/python --version
 ${VENV_BIN_DIR}/python -m pip --version
+# install dependencies in the venv
+${VENV_BIN_DIR}/python -m pip install six protobuf
 
 PYTHONPATH=ortools ${VENV_BIN_DIR}/python -c "import ortools"
-PYTHONPATH=ortools ${VENV_BIN_DIR}/python -v -c "from ortools.linear_solver import pywraplp"
+PYTHONPATH=ortools ${VENV_BIN_DIR}/python -c "from ortools.linear_solver import pywraplp"
 
 cp test.py.in ${VENV_DIR}/test.py
-PYTHONPATH=ortools ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py
+PYTHONPATH=. ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py
+
+rm -rf ${VENV_DIR}
 #ldd ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
 #objdump -p ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
 #make install_python
