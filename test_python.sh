2b821ae9c4280eea060bbcb1cf55d351a7f11072
Corentin Le Molgat <corentinl@google.com>
2018-05-22 13:16:33 +0200

    Update test_python.sh

--- test_python.sh
+++ test_python.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 set -xe
 
-VENV_DIR=venv
-rm -rf ${VENV_DIR}
-python -m virtualenv -p python ${VENV_DIR}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VENV_DIR="${DIR}/venv"
+rm -rf "${VENV_DIR}"
+python -m virtualenv -p python "${VENV_DIR}"
 
-VENV_BIN_DIR=${VENV_DIR}/bin
-${VENV_BIN_DIR}/python --version
-${VENV_BIN_DIR}/python -m pip --version
+source "${VENV_DIR}/bin/activate"
+python --version
+python -m pip --version
 # install dependencies in the venv
-${VENV_BIN_DIR}/python -m pip install six protobuf
+python -m pip install six protobuf
 
-PYTHONPATH=ortools ${VENV_BIN_DIR}/python -c "import ortools"
-PYTHONPATH=ortools ${VENV_BIN_DIR}/python -c "from ortools.linear_solver import pywraplp"
+# Test ortools python from the build dir
+python -c "import ortools"
+python -c "from ortools.linear_solver import pywraplp"
+cp test.py.in "${VENV_DIR}/test.py"
+PYTHONPATH=. python "${VENV_DIR}/test.py"
 
-cp test.py.in ${VENV_DIR}/test.py
-PYTHONPATH=. ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py
+# Try the wheel package
+PYTHON_VERSION="$(python -c "from sys import version_info as v; print(str(v[0])+'.'+str(v[1]))")"
+WHEEL_PKG="$(find "temp-python${PYTHON_VERSION}/ortools/dist/" -name "ortools-*.whl")"
+python -m pip install "${WHEEL_PKG}"
+python "${VENV_DIR}/test.py"
 
-rm -rf ${VENV_DIR}
+# Cleaning
+deactivate
+rm -rf "${VENV_DIR}"
+
+# Misc
 #ldd ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
 #objdump -p ortools/linear_solver/../gen/ortools/linear_solver/_pywraplp.so
 #make install_python
